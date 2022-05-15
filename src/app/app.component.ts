65525a0f8762ea0ec62131a6d8786468bb3c9553
inspiringsource <ab.b2k3@gmail.com>
2022-05-15 17:44:45 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crypto-finance-challenge';
+  btcPriceStart: number = 30022.69; //current bitcoin price 15.2.22 - the starting price.
+  btcPriceChange: number = 0;
+  btcPrice: number = this.btcPriceStart;
+  cashOnHand: number = 100000;
+  bitcoinOwned: number = 0;
+  currentTracsaction: number | string = 0;
+  amount: string[] | number = [];
+  currentTracsactions: string[] = [];
+  buyingPriceA: number = 0;
+  buyingPriceQ: number = 0;
+
+  constructor() {
+    this.timeOut()
+  }
+
+  // This function is called every second to update the bitcoin price
+  timeOut() {
+    setTimeout(() => {
+      this.btcPriceChange = ((Math.random())*2-1)*10
+      this.btcPrice = this.btcPriceStart + this.btcPriceChange
+      this.timeOut()
+    }, 1000)
+  }
+  onInput(amount: number | string) {
+    this.currentTracsaction = amount;
+    this.buyingPriceA = 1
+    // console.log(Number(this.buyingPriceA))
+  }
+  
+  // quantity of bitcoin to buy
+    onQuantity(quantityBtc: number | string) {
+      this.currentTracsaction = quantityBtc;
+      this.buyingPriceQ = 1
+      console.log(this.currentTracsaction)
+    }
+
+  // buy transaction
+  onBuy() {
+    if (
+       (this.cashOnHand < 0)
+
+        ) {
+            return alert("Not sufficient funds")
+      } else {
+        if ((this.buyingPriceA > 0) && (this.cashOnHand >= 0) && (this.currentTracsaction <= this.cashOnHand)) {
+          this.buyingPriceA = this.btcPrice  
+          this.currentTracsactions.push(String((Number(this.currentTracsaction)).toLocaleString('de-CH')))
+        this.bitcoinOwned += (Number(this.currentTracsaction)/this.buyingPriceA);
+        this.cashOnHand -= Number(this.currentTracsaction);
+        console.log(Number(this.buyingPriceA))
+        this.buyingPriceA = 0;
+   
+      
+        } 
+        if ((this.buyingPriceQ > 0)&& (this.cashOnHand >= 0) && (Number(this.currentTracsaction)*this.btcPrice <= this.cashOnHand)) {
+          this.buyingPriceQ = this.btcPrice
+          this.currentTracsactions.push(String((Number(this.currentTracsaction)*this.btcPrice).toLocaleString('de-CH')))
+          this.bitcoinOwned += (Number(this.currentTracsaction));
+          this.cashOnHand -= Number(this.currentTracsaction)*this.buyingPriceQ;
+          this.buyingPriceQ = 0;
+  
+          } 
+      }
+      
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    NoopAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
